a01daee58478cfc1f7721ab883de4b62f24a8c2d
magicary <behnke.ca@gmail.com>
2024-03-25 19:10:01 -0500

    fixed small error in schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,23 @@ CREATE DATABASE company_db;
 \c company_db
 
 CREATE TABLE department (
-    id SERIAL PRIMARY KEY,
-    department_name VARCHAR(30)
+    id SERIAL PRIMARY KEY, 
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id SERIAL PRIMARY KEY,
-    title   VARCHAR(30),
-    salary DECIMAL,
-    department INTEGER,
+    title   VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department INTEGER NOT NULL,
     FOREIGN KEY (department) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INTEGER,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
     manager INTEGER,
     FOREIGN KEY (role_id) REFERENCES roles(id)
     FOREIGN KEY (manager) REFERENCES employee(id)
